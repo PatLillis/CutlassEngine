74ae29907a11bd9d3be879e03ca046f093cd8630
Pat Lillis <lillispm@gmail.com>
2014-04-21 19:59:20 -0400

    New TextureManager
    -TextureManager now uses TexId struct, instead of by string.
    -This guarantees no collisions of Texture names, and makes maintainance
    easier.
    -TODO: Also update FontManager?

---
+++ CutlassEngine/CutlassEngine/Utilities/Structs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cutlass.Utilities
+{
+    public struct TexId
+    {
+        private int Id;
+
+        public TexId(int id)
+        {
+            Id = id;
+        }
+
+        public static implicit operator TexId(int id)
+        {
+            return new TexId(id);
+        }
+
+        public static implicit operator int(TexId id)
+        {
+            return id.Id;
+        }
+    }
+}
